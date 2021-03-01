407fc1cbe079a9c7f59d644a110730571435581f
AndreyPootMay <spocker312@gmail.com>
2021-02-28 19:15:42 -0500

    feat: :wrench: First commit
    In this commit I upload all the files from my first gridsome app example.
    
    #1

---
+++ src/pages/Projects.vue
@@ -0,0 +1,20 @@
+<template>
+  <BlogLayout>
+    <h1>Projects</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque
+      omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum
+      odit fugit assumenda rem dolores inventore iste reprehenderit maxime!
+      Iusto.
+    </p>
+  </BlogLayout>
+</template>
+
+<script>
+
+export default {
+  metaInfo: {
+    title: "Projects",
+  },
+};
+</script>
